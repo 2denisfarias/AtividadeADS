b0e36950f94064ad38c04c09b3550db15c5e6f01
2denisfarias <2denisfarias@gmail.com>
2023-02-24 21:27:31 -0300

    Projeto java

---
+++ src/atividade/Laranja.java
@@ -0,0 +1,17 @@
+package atividade;
+
+public class Laranja {
+	public static void main(String[] args) {
+		
+	 int laranja = 60;
+	 int divisao = 60/4;
+		
+	 
+	 
+	 System.out.println("João tem 60 laranjas, e vai dividir com seus amigos");
+	 System.out.println("Como ele tem 3 amigos, Pedro, Mateus e Lucas, essa divisão será:");
+	 System.out.println("A divisão correta entre todos os 4, será: " + divisao);
+	 
+	}
+
+}
